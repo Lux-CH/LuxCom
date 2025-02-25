f9556f1be703f44a69490ae3546030e7054cb2c9
c22dev <102235607+c22dev@users.noreply.github.com>
2025-02-25 14:27:21 +0100

    made a func public

--- Sources/LuxCom/Trip/TripPlanner.swift
+++ Sources/LuxCom/Trip/TripPlanner.swift
@@ -8,9 +8,14 @@
 import Foundation
 import OJP
 
-func findTrip(ojpConfig: OJP, from: OJPv2.PlaceRefChoice, via: [OJPv2.PlaceRefChoice]? = nil, to: OJPv2.PlaceRefChoice) async throws -> OJPv2.TripDelivery {
+public func findTrip(ojpConfig: OJP, from: OJPv2.PlaceRefChoice, via: [OJPv2.PlaceRefChoice]? = nil, to: OJPv2.PlaceRefChoice) async throws -> OJPv2.TripDelivery {
     if let viaDest = via {
         return try await ojpConfig.requestTrips(from: from, to: to, via: viaDest, params: .init())
     }
     return try await ojpConfig.requestTrips(from: from, to: to, params: .init())
 }
+// MARK: idk if i should add a share trip feature, might be intersting ngl..
+// i'll have to figure out TripInfoDel vs TripDel, might be able to get tripdel through TripInfoDel
+//public func requestTrip(ojpConfig: OJP, ref: String, operatingDay: String) async throws -> OJPv2.TripInfoDelivery {
+//    return try await ojpConfig.requestTripInfo(journeyRef: ref, operatingDayRef: operatingDay)
+//}
