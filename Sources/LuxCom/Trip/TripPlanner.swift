5b48087610b11ed57bec38826c8107f8a0bfc54a
c22dev <102235607+c22dev@users.noreply.github.com>
2025-02-25 15:10:29 +0100

    uncommented, i was wrong on the nature of the func

--- Sources/LuxCom/Trip/TripPlanner.swift
+++ Sources/LuxCom/Trip/TripPlanner.swift
@@ -14,8 +14,7 @@ public func findTrip(ojpConfig: OJP, from: OJPv2.PlaceRefChoice, via: [OJPv2.Pla
     }
     return try await ojpConfig.requestTrips(from: from, to: to, params: .init())
 }
-// MARK: idk if i should add a share trip feature, might be intersting ngl..
-// i'll have to figure out TripInfoDel vs TripDel, might be able to get tripdel through TripInfoDel
-//public func requestTrip(ojpConfig: OJP, ref: String, operatingDay: String) async throws -> OJPv2.TripInfoDelivery {
-//    return try await ojpConfig.requestTripInfo(journeyRef: ref, operatingDayRef: operatingDay)
-//}
+
+public func requestTripInfo(ojpConfig: OJP, ref: String, operatingDay: String) async throws -> OJPv2.TripInfoDelivery {
+    return try await ojpConfig.requestTripInfo(journeyRef: ref, operatingDayRef: operatingDay)
+}
