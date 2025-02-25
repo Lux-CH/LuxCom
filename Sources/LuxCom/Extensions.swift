462dd58b5577d51b02c065488b2a63d842a40308
c22dev <102235607+c22dev@users.noreply.github.com>
2025-02-25 14:12:34 +0100

    add extensions from OJP-SampleApp

---
+++ Sources/LuxCom/Extensions.swift
@@ -0,0 +1,132 @@
+//
+//  Extensions.swift
+//  LuxCom
+//
+//  Created by Constantin Clerc on 24.02.2025.
+//
+//  Source : https://github.com/openTdataCH/ojp-ios/blob/69e0434290c5fe5bd368b996b0c4ac667148398f/SampleApp/OJPSampleApp/Helpers/OJP%2BExtensions.swift
+
+import Foundation
+import OJP
+
+extension OJPv2.LegIntermediate: @retroactive Identifiable {
+    public var id: String {
+        stopPointRef
+    }
+}
+
+extension OJPv2.PTSituation {
+    var allInfos: [String] {
+        var infos: [String] = []
+        guard let publishingActions else { return [] }
+        for publishingAction in publishingActions.publishingActions {
+            for passengerInformationAction in publishingAction.passengerInformationActions {
+                for textualContent in passengerInformationAction.textualContents {
+                    infos.append(textualContent.summaryContent.summaryText)
+
+                    for descriptionContent in textualContent.descriptionContents {
+                        infos.append(descriptionContent.descriptionText)
+                    }
+
+                    for consequenceContent in textualContent.consequenceContents {
+                        infos.append(consequenceContent.consequenceText)
+                    }
+
+                    for recommendationContent in textualContent.recommendationContents {
+                        infos.append(recommendationContent.recommendationText)
+                    }
+
+                    for remarkContent in textualContent.remarkContents {
+                        infos.append(remarkContent.remarkText)
+                    }
+
+                    if let reasonContent = textualContent.reasonContent {
+                        infos.append(reasonContent.reasonText)
+                    }
+
+                    if let durationContent = textualContent.durationContent {
+                        infos.append(durationContent.durationText)
+                    }
+                }
+            }
+        }
+        return infos
+    }
+}
+
+extension OJPv2.TripStatus {
+    var hasIssue: Bool {
+        cancelled || infeasible || deviation
+    }
+
+    var title: String {
+        if cancelled {
+            "Cancelled"
+        } else if infeasible {
+            "Infeasible"
+        } else if deviation {
+            "Deviation"
+        } else {
+            ""
+        }
+    }
+}
+
+extension OJPv2.AlertCause {
+    var title: String {
+        switch self {
+        case .undefinedAlertCause:
+            "Undefiniert"
+        case .constructionWork:
+            "Baustelle"
+        case .serviceDisruption:
+            "Unterbruch"
+        case .emergencyServicesCall:
+            "Notfall-Einsatz"
+        case .vehicleFailure:
+            "Fahrzeugstörung"
+        case .poorWeather:
+            "Unwetter"
+        case .routeBlockage:
+            "Blockierte Strecke"
+        case .technicalProblem:
+            "Technisches Problem"
+        case .unknown:
+            "Unbekannt"
+        case .accident:
+            "Unfall"
+        case .specialEvent:
+            "Special Event"
+        case .congestion:
+            "Stau"
+        case .maintenanceWork:
+            "Unterhaltsarbeiten"
+        }
+    }
+}
+
+extension OJPv2.Trip {
+    var firstTimedLeg: OJPv2.TimedLeg? {
+        legs.compactMap({ leg in
+            if case let .timed(timedLeg) = leg.legType {
+                return timedLeg
+            }
+            return nil }
+        ).first
+    }
+
+    var lastTimedLeg: OJPv2.TimedLeg? {
+        legs.compactMap({ leg in
+            if case let .timed(timedLeg) = leg.legType {
+                return timedLeg
+            }
+            return nil }
+        ).last
+    }
+}
+
+extension OJPv2.TripInfoResult: @retroactive Identifiable {
+    public var id: Int {
+        service?.journeyRef.hashValue ?? (previousCalls.hashValue + onwardCalls.hashValue)
+    }
+}
