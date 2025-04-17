91bcbdef802af3fd0861d7762971537462979bab
c22dev <102235607+c22dev@users.noreply.github.com>
2025-04-17 08:36:55 +0200

    bias geocoding

--- Sources/LuxCom/Search/Geocode.swift
+++ Sources/LuxCom/Search/Geocode.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public func geocode(text: String, language: String = "fr", type: LocationType? = nil) async throws -> [SearchResult] {
+public func geocode(text: String, language: String = "fr", type: LocationType? = nil, place: (Double, Double)? = nil, placeBias: Int? = nil) async throws -> [SearchResult] {
     var queryItems = [
         URLQueryItem(name: "text", value: text),
         URLQueryItem(name: "language", value: language)
@@ -15,6 +15,14 @@ public func geocode(text: String, language: String = "fr", type: LocationType? =
     
     if let locType = type {
         queryItems.append(URLQueryItem(name: "type", value: locType.rawValue))
+    }
+    
+    if let specifiedPlace = place {
+        queryItems.append(URLQueryItem(name: "place", value: "\(specifiedPlace.0), \(specifiedPlace.1)"))
+    }
+    
+    if let specifiedPlaceBias = placeBias {
+        queryItems.append(URLQueryItem(name: "placeBias", value: String(specifiedPlaceBias)))
     }
     
     return try await APIClient.fetch(from: "/geocode", queryItems: queryItems)
