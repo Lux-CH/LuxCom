0532b2501385f7da8a39e38135172b526b518af5
c22dev <102235607+c22dev@users.noreply.github.com>
2025-04-27 21:16:22 +0200

    added an init on stoptinmes

--- Sources/LuxCom/Models/Stop/StopTimes.swift
+++ Sources/LuxCom/Models/Stop/StopTimes.swift
@@ -9,4 +9,10 @@ public struct StopTimes: Codable, Sendable {
     public let stopTimes: [StopTime]
     public let previousPageCursor: String
     public let nextPageCursor: String
+    
+    init(stopTimes: [StopTime], previousPageCursor: String, nextPageCursor: String) {
+        self.stopTimes = stopTimes
+        self.previousPageCursor = previousPageCursor
+        self.nextPageCursor = nextPageCursor
+    }
 }
