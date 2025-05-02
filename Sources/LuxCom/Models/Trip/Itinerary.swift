8c6b9c9585f5f6d3f402252f604c9daf02241c7c
c22dev <102235607+c22dev@users.noreply.github.com>
2025-05-03 00:32:59 +0200

    made itinerary equatable

--- Sources/LuxCom/Models/Trip/Itinerary.swift
+++ Sources/LuxCom/Models/Trip/Itinerary.swift
@@ -7,10 +7,19 @@
 
 import Foundation
 
-public struct Itinerary: Codable, Sendable {
+public struct Itinerary: Codable, Sendable, Equatable {
     public let duration: Int
     public let startTime: Date
     public let endTime: Date
     public let transfers: Int
     public let legs: [Leg]
+    
+    // prob not the best way but uhm
+    public static func == (lhs: Itinerary, rhs: Itinerary) -> Bool {
+        return lhs.startTime == rhs.startTime &&
+        lhs.endTime == rhs.endTime &&
+        lhs.duration == rhs.duration &&
+        lhs.transfers == rhs.transfers &&
+        lhs.legs.count == rhs.legs.count
+    }
 }
