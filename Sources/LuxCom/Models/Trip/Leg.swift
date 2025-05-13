70c9c2eef8e420329de9be2444b6f086e424bc98
c22dev <102235607+c22dev@users.noreply.github.com>
2025-05-13 18:28:20 +0200

    leg init

--- Sources/LuxCom/Models/Trip/Leg.swift
+++ Sources/LuxCom/Models/Trip/Leg.swift
@@ -24,4 +24,22 @@ public struct Leg: Codable, Sendable {
     public let legGeometry: LegGeometry
     public let agencyId: String?
     public let steps: [StepInstruction]?
+    public init(mode: TransportationMode, from: Place, to: Place, duration: Int, startTime: Date, endTime: Date, scheduledStartTime: Date, scheduledEndTime: Date, realTime: Bool, distance: Double?, headsign: String?, routeShortName: String?, intermediateStops: [Place]?, legGeometry: LegGeometry, agencyId: String?, steps: [StepInstruction]?) {
+        self.mode = mode
+        self.from = from
+        self.to = to
+        self.duration = duration
+        self.startTime = startTime
+        self.endTime = endTime
+        self.scheduledStartTime = scheduledStartTime
+        self.scheduledEndTime = scheduledEndTime
+        self.realTime = realTime
+        self.distance = distance
+        self.headsign = headsign
+        self.routeShortName = routeShortName
+        self.intermediateStops = intermediateStops
+        self.legGeometry = legGeometry
+        self.agencyId = agencyId
+        self.steps = steps
+    }
 }
