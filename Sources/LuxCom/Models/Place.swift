ea009b78786f2b1d477e9719bb0582c1aafa56e0
c22dev <102235607+c22dev@users.noreply.github.com>
2025-07-08 03:36:13 +0200

    attempt to use official tracks

--- Sources/LuxCom/Models/Place.swift
+++ Sources/LuxCom/Models/Place.swift
@@ -19,18 +19,7 @@ public struct Place: Codable, Hashable, Sendable, Equatable, Identifiable {
     public let scheduledArrival: Date?
     public let scheduledDeparture: Date?
     public let scheduledTrack: String?
-    private let _track: String?
-    
-    // The track is determined by the stopID (as provided by OTD)
-    // Example format : odchgtfs_0000000:LEVEL:TRACK
-    // Example : odchgtfs_8592679:0:F
-    // F here is the track. Thus we extract this.
-    // In case something bad occurs, we fallback to the original track attrib (here mapped on _track) from API.
-    
-    public var track: String? {
-        let trackValue = stopId?.components(separatedBy: ":").last ?? _track
-        return trackValue?.hasPrefix("ch_") == true ? nil : trackValue
-    }
+    public let track: String?
     
     public let vertexType: VertexType
     
@@ -43,10 +32,10 @@ public struct Place: Codable, Hashable, Sendable, Equatable, Identifiable {
     public enum CodingKeys: String, CodingKey {
         case name, stopId, lat, lon, level, arrival, departure
         case scheduledArrival, scheduledDeparture, scheduledTrack
-        case _track = "track"
+        case track = "track"
         case vertexType
     }
-    public init(name: String, stopId: String?, lat: Double, lon: Double, level: Double, arrival: Date?, departure: Date?, scheduledArrival: Date?, scheduledDeparture: Date?, scheduledTrack: String?, _track: String?, vertexType: VertexType) {
+    public init(name: String, stopId: String?, lat: Double, lon: Double, level: Double, arrival: Date?, departure: Date?, scheduledArrival: Date?, scheduledDeparture: Date?, scheduledTrack: String?, track: String?, vertexType: VertexType) {
         self.name = name
         self.stopId = stopId
         self.lat = lat
@@ -57,7 +46,7 @@ public struct Place: Codable, Hashable, Sendable, Equatable, Identifiable {
         self.scheduledArrival = scheduledArrival
         self.scheduledDeparture = scheduledDeparture
         self.scheduledTrack = scheduledTrack
-        self._track = _track
+        self.track = track
         self.vertexType = vertexType
     }
 }
