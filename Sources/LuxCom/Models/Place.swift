6899f4e10a624f163944fcaf627143e86cf15959
Constantin Clerc <constclerc@gmail.com>
2025-08-18 01:37:27 +0200

    remove alerts (useless)

--- Sources/LuxCom/Models/Place.swift
+++ Sources/LuxCom/Models/Place.swift
@@ -14,12 +14,10 @@ public struct Place: Codable, Hashable, Sendable, Equatable, Identifiable {
     public let lat: Double
     public let lon: Double
     public let level: Double
-    public let alerts: [Alert]?
     public let arrival: Date?
     public let departure: Date?
     public let scheduledArrival: Date?
     public let scheduledDeparture: Date?
-    public let cancelled: Bool?
     public let scheduledTrack: String?
     public let track: String?
     
@@ -32,23 +30,21 @@ public struct Place: Codable, Hashable, Sendable, Equatable, Identifiable {
     }
     
     public enum CodingKeys: String, CodingKey {
-        case name, stopId, lat, lon, level, arrival, departure, cancelled
+        case name, stopId, lat, lon, level, arrival, departure
         case scheduledArrival, scheduledDeparture, scheduledTrack
-        case track, vertexType, alerts
+        case track = "track"
+        case vertexType
     }
-
-    public init(name: String, stopId: String?, lat: Double, lon: Double, level: Double, alerts: [Alert]?, arrival: Date?, departure: Date?, scheduledArrival: Date?, scheduledDeparture: Date?, cancelled: Bool?, scheduledTrack: String?, track: String?, vertexType: VertexType) {
+    public init(name: String, stopId: String?, lat: Double, lon: Double, level: Double, arrival: Date?, departure: Date?, scheduledArrival: Date?, scheduledDeparture: Date?, scheduledTrack: String?, track: String?, vertexType: VertexType) {
         self.name = name
         self.stopId = stopId
         self.lat = lat
         self.lon = lon
         self.level = level
-        self.alerts = alerts
         self.arrival = arrival
         self.departure = departure
         self.scheduledArrival = scheduledArrival
         self.scheduledDeparture = scheduledDeparture
-        self.cancelled = cancelled
         self.scheduledTrack = scheduledTrack
         self.track = track
         self.vertexType = vertexType
