0b5f91c3de19c1a94ac808c272203ae7b32b6e69
c22dev <102235607+c22dev@users.noreply.github.com>
2025-09-22 22:34:26 +0200

    use double instead of int for pedestrianspeed

--- Sources/LuxCom/Models/Trip/RouteOptions.swift
+++ Sources/LuxCom/Models/Trip/RouteOptions.swift
@@ -36,7 +36,7 @@ public struct RouteOptions: Sendable {
     public let maxTransfers: Int
     public let minTransferTime: Int
     public let pedestrianProfile: PedestrianProfile
-    public let pedestrianSpeed: Int?
+    public let pedestrianSpeed: Double?
     public let useRoutedTransfers: Bool? = true
     public let maxMatchingDistance: Int? = 250
     public let detailedTransfers: Bool? = true
@@ -71,7 +71,7 @@ public struct RouteOptions: Sendable {
         }
     }
     
-    public init(from: RouteLocation, to: RouteLocation, via: [String]?, viaMinimumStay: [Int], time: Date?, arriveBy: Bool, maxTransfers: Int, minTransferTime: Int, pedestrianProfile: PedestrianProfile, pedestrianSpeed: Int?, transitModes: [TransportationMode]?, numItineraries: Int?, pageCursor: String? = nil, timetableView: Bool, maxPreTransitTime: Int?, maxPostTransitTime: Int?) {
+    public init(from: RouteLocation, to: RouteLocation, via: [String]?, viaMinimumStay: [Int], time: Date?, arriveBy: Bool, maxTransfers: Int, minTransferTime: Int, pedestrianProfile: PedestrianProfile, pedestrianSpeed: Double?, transitModes: [TransportationMode]?, numItineraries: Int?, pageCursor: String? = nil, timetableView: Bool, maxPreTransitTime: Int?, maxPostTransitTime: Int?) {
         self.from = from
         self.to = to
         self.via = via
