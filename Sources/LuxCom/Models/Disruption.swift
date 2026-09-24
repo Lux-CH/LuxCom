d8e3f22da6eebbb48d7be9acb1ff23e2bd23a950
c22dev <102235607+c22dev@users.noreply.github.com>
2026-09-24 11:29:25 +0200

    group sbb disruption trips into tripIds

--- Sources/LuxCom/Models/Disruption.swift
+++ Sources/LuxCom/Models/Disruption.swift
@@ -12,14 +12,6 @@ public struct Disruption: Identifiable, Hashable, Sendable, Decodable {
     public let lineBackground: String // HEX
     public let lineDisruption: String
     public let agencyId: String?
-    public let tripId: String?
+    public let tripIds: [String]?
     public var id: String { lineDisruption }
-
-    public init(line: String, lineBackground: String, lineDisruption: String, agencyId: String? = nil, tripId: String? = nil) {
-        self.line = line
-        self.lineBackground = lineBackground
-        self.lineDisruption = lineDisruption
-        self.agencyId = agencyId
-        self.tripId = tripId
-    }
 }
